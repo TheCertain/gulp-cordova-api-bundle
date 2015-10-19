c046143f2f2cd239ac50f646d40ff8731ca775ea
TheCertain <github@xelpara.de>
2015-10-19 22:17:13 +0200

    Initial commit of main files

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+   create: require('gulp-cordova-create'),
+   plugin: function() {
+      var p = require('gulp-cordova-plugin');
+      p.remove = require('gulp-cordova-plugon-remove');
+      return p;
+   }(),
+   preference: require('gulp-cordova-preference'),
+   access: require('gulp-cordova-access'),
+   version: require('gulp-cordova-version'),
+   description: require('gulp-cordova-description'),
+   author: require('gulp-cordova-author'),
+   icon: require('gulp-cordova-icon'),
+   xml: require('gulp-cordova-xml'),
+   build: {
+      ios: require('gulp-cordova-build-ios'),
+      android: require('gulp-cordova-build-android'),
+      bb10: require('gulp-cordova-build-bb10')
+   }
+};
